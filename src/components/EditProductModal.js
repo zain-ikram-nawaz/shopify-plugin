a85a03ad7ec7f03de6392cb4f0424b4b1b950b75
zain-ikram-nawaz <zainikram704@gmail.com>
2025-08-18 14:30:18 +0500

    sdsd

--- src/components/EditProductModal.js
+++ src/components/EditProductModal.js
@@ -67,8 +67,6 @@ const handleSubmit = async (e) => {
 
     const result = await response.json();
     if (!response.ok) throw new Error(result.error || "Update failed");
-
-    onUpdate(result.product);
     onClose();
   } catch (err) {
     console.error("Update error:", err);
